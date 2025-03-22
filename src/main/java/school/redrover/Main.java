67298c2d147e79a50558060024b302198bdf96db
DMM <poolool@mail.ru>
2025-03-21 22:53:38 -0400

    first commit

---
+++ src/main/java/school/redrover/Main.java
@@ -0,0 +1,11 @@
+package school.redrover;
+
+public class Main {
+
+    public static int getSum(int a , int b){
+        return a + b;
+    }
+    public static String getConcat (String str1, String str2){
+        return str1.concat(str2);
+    }
+}
